0e497e0cf6f2e43f2467c774f9fb532327804029
U-Sasuke <yaoqing1116@163.com>
2017-12-03 14:13:40 +0800

    初始化module

---
+++ callme-start/src/main/java/com/small/group/sfy/CallmeStartApplication.java
@@ -0,0 +1,12 @@
+package com.small.group.sfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallmeStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallmeStartApplication.class, args);
+	}
+}
